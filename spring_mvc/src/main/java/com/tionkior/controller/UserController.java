e7089384c9661a83f2177d1fbb4519bf3810cb89
TionKior <1423304936@qq.com>
2021-06-06 13:08:45 +0800

    feat: SpringMVC练习

---
+++ spring_mvc/src/main/java/com/tionkior/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.tionkior.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @ClassName : UserController
+ * @Author : TionKior
+ * @Date : 2021/6/6 10:42
+ * @Version : V1.0
+ * @Description :
+ */
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    // 请求地址 http://localhost:8080/user/quick
+    @RequestMapping(value = "/quick", method = RequestMethod.GET, params = {"username"})
+    public String save() {
+        System.out.println("Controller save running...");
+        return "success";
+    }
+
+}
