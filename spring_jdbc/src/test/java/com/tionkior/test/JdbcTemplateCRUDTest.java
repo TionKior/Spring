72db2ca7841bcde35e3f02996c9a67cf8b33e35b
TionKior <1423304936@qq.com>
2021-06-13 09:59:21 +0800

    feat: Spring_JDBC练习,Spring-test测试JdbcTemplate,查询操作

--- spring_jdbc/src/test/java/com/tionkior/test/JdbcTemplateCRUDTest.java
+++ spring_jdbc/src/test/java/com/tionkior/test/JdbcTemplateCRUDTest.java
@@ -1,11 +1,15 @@
 package com.tionkior.test;
 
+import com.tionkior.domain.Account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 /**
  * @ClassName : JdbcTemplateCRUDTest
@@ -22,13 +26,31 @@ public class JdbcTemplateCRUDTest {
     private JdbcTemplate jdbcTemplate;
 
     @Test
+    public void testQueryCount() {
+        Long count = jdbcTemplate.queryForObject("select count(*) from account", Long.class);
+        System.out.println(count);
+    }
+
+    @Test
+    public void testQueryOne() {
+        Account account = jdbcTemplate.queryForObject("select * from account where name = ?", new BeanPropertyRowMapper<Account>(Account.class), "tom");
+        System.out.println(account);
+    }
+
+    @Test
+    public void testQueryAll() {
+        List<Account> accountList = jdbcTemplate.query("select * from account", new BeanPropertyRowMapper<Account>(Account.class));
+        System.out.println(accountList);
+    }
+
+    @Test
     public void testUpdate() {
         jdbcTemplate.update("update account set money = ? where name = ?", 10000, "tom");
     }
 
     @Test
     public void testDelete() {
-        jdbcTemplate.update("delete from account where name = ?", "tom");
+        jdbcTemplate.update("delete from account where name = ?", "lisi");
     }
 
 }
