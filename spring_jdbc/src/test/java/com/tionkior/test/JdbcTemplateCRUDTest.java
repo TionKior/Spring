d36027c953d95669357513497a097ad3c0409d8f
TionKior <1423304936@qq.com>
2021-06-13 09:15:19 +0800

    feat: Spring_JDBC练习,Spring产生JdbcTemplate对象,抽取properties

---
+++ spring_jdbc/src/test/java/com/tionkior/test/JdbcTemplateCRUDTest.java
@@ -0,0 +1,13 @@
+package com.tionkior.test;
+
+/**
+ * @ClassName : JdbcTemplateCRUDTest
+ * @Author : TionKior
+ * @Date : 2021/6/13 9:13
+ * @Version : V1.0
+ * @Description : Spring集成Junit测试
+ */
+
+
+public class JdbcTemplateCRUDTest {
+}
