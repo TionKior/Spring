eb0d22ccafbe62e4878f64b6addb6ed9fa6cd0f5
TionKior <1423304936@qq.com>
2021-06-16 10:49:00 +0800

    feat: Spring练习,AOP,面向切面编程

--- spring_aop/src/main/java/com/tionkior/aop/Target.java
+++ spring_aop/src/main/java/com/tionkior/aop/Target.java
@@ -11,7 +11,7 @@ package com.tionkior.aop;
 public class Target implements TargetInterface {
     @Override
     public void save() {
-        int i = 1 / 0;
+        // int i = 1 / 0;
         System.out.println("save running...");
     }
 }
