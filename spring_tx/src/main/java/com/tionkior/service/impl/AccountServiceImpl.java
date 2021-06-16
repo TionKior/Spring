22ae1d7bda227b92ee6d5ef5c2e5eac944bff7fa
TionKior <1423304936@qq.com>
2021-06-16 13:16:38 +0800

    feat: Spring练习,事务控制

--- spring_tx/src/main/java/com/tionkior/service/impl/AccountServiceImpl.java
+++ spring_tx/src/main/java/com/tionkior/service/impl/AccountServiceImpl.java
@@ -13,6 +13,7 @@ public class AccountServiceImpl implements AccountService {
 
     public void transfer(String outMan, String inMan, double money) {
         accountDao.out(outMan, money);
+        int i = 1 / 0;
         accountDao.in(inMan, money);
     }
 }
