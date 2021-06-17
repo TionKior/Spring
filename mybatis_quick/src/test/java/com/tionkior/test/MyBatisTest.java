3c32a35388c7c81c2ac1f40ad68b1edc4dcfcab1
TionKior <1423304936@qq.com>
2021-06-17 10:44:26 +0800

    feat: MyBatis练习,更新操作

--- mybatis_quick/src/test/java/com/tionkior/test/MyBatisTest.java
+++ mybatis_quick/src/test/java/com/tionkior/test/MyBatisTest.java
@@ -22,6 +22,58 @@ import java.util.List;
 public class MyBatisTest {
 
     @Test
+    // 修改操作
+    public void test3() throws IOException {
+
+        // 模拟user对象
+        User user = new User();
+        user.setId(7);
+        user.setUsername("lucy");
+        user.setPassword("123");
+
+        // 获得核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        // 获得session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获得session会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 执行操作 参数：namespace+id
+        sqlSession.update("userMapper.update", user);
+
+        // mybatis执行更新操作 提交事务
+        sqlSession.commit();
+
+        // 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    // 插入操作
+    public void test2() throws IOException {
+
+        // 模拟user对象
+        User user = new User();
+        user.setUsername("tom");
+        user.setPassword("abc");
+
+        // 获得核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        // 获得session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获得session会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 执行操作 参数：namespace+id
+        sqlSession.insert("userMapper.save", user);
+
+        // mybatis执行更新操作 提交事务
+        sqlSession.commit();
+
+        // 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    // 查询操作
     public void test1() throws IOException {
         // 获得核心配置文件
         InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
@@ -35,6 +87,5 @@ public class MyBatisTest {
         System.out.println(userList);
         // 释放资源
         sqlSession.close();
-
     }
 }
