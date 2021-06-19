fdc19a8857f1e97f84b7781c8811892bb3884939
TionKior <1423304936@qq.com>
2021-06-19 08:28:08 +0800

    feat: MyBatis练习,多表查询

---
+++ mybatis_multi/src/main/java/com/tionkior/mapper/OrderMapper.java
@@ -0,0 +1,6 @@
+package com.tionkior.mapper;
+
+public interface OrderMapper {
+
+
+}
---
+++ mybatis_multi/src/main/java/com/tionkior/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.tionkior.mapper;
+
+/**
+ * @ClassName : UserMapper
+ * @Author : TionKior
+ * @Date : 2021/6/19 8:17
+ * @Version : V1.0
+ * @Description :
+ */
+
+public class UserMapper {
+
+
+}
